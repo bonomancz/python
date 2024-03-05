b3e91ad2bba6fe635efd6ed49b9d9db15fa5ec9d
bonoman@volny.cz <bonoman@volny.cz>
2024-03-05 23:55:09 +0100

    develop 20240305

--- jsonSocketAgent/classOdbc.py
+++ jsonSocketAgent/classOdbc.py
@@ -1,4 +1,60 @@
-import odbc
 
-class dbOdbc():
-    pass
+"""
+ODBC connection, query class
+Uses pyodbc module.
+Author: Jan Novotny (2024)
+Mail: jan.novotny.cz@gmail.com
+"""
+
+import pyodbc
+
+class DBOdbc():
+    
+    def __init__(self):
+        ''' default constructor '''
+        self.connection = None
+        self.cursor = None
+
+    
+    def connect(self, connectionString):
+        ''' ODBC connect function '''
+        retVal = False
+        try:
+            self.connection = pyodbc.connect(connectionString)
+            self.cursor = self.connection.cursor()
+            retVal = True
+        except pyodbc.Error as e:
+            print(f"Method: connect(): Exception: {e}")
+            retVal = False
+            raise e
+        return retVal
+
+
+    def query(self, queryString):
+        ''' ODBC send query function '''
+        retVal = None
+        try:
+            print(self.getQueryType(queryString))
+            if self.cursor.execute(queryString):
+                retVal = self.cursor.fetchall()
+        except pyodbc.Error as e:
+            print(f"Method: query(): Exception: {e}")
+            retVal = None
+            raise e
+        return retVal
+    
+
+    def disconnect(self):        
+        ''' ODBC close function '''
+        retVal = False
+        if self.connection:
+            if self.connection.close():
+                retVal = True
+        return retVal
+
+
+    def getQueryType(self, queryString):
+        retVal = ""
+        keyWord = queryString.split(" ")
+        retVal = keyWord[0]
+        return retVal
--- jsonSocketAgent/jsonSocketAgent.py
+++ jsonSocketAgent/jsonSocketAgent.py
@@ -1,11 +1,35 @@
-# JSON socket agent
-# Agent receives json files and stores data to database server
-# author: jan.novotny.cz@gmail.com
-# 2.3.2024
+"""
+JSON socket agent
+Agent receives json file.s and stores data to database server
+Author: jan.novotny.cz@gmail.com
+Dev. started: 2.3.2024
+"""
 
+
+from classOdbc import DBOdbc
+from classSocket import Socket
 
 def main():
-    pass
+    ipAddress = "127.0.0.1"
+    db = DBOdbc()
+    sock = Socket(ipAddress)
+
+    # DB connection parameters
+    dsn = "MariaDB"
+    server = "192.168.240.50"
+    database = "asterisk"
+    username = "lelek"
+    password = "Dbuser*9999"
+    connectionString = f"DSN={dsn};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+
+    try:
+        db.connect(connectionString)
+        queryString = "select exten, context from asterisk.extensions order by exten, priority;"
+        print(db.query(queryString))
+    except Exception as e:
+        print(f"Exception: {e}")
+    finally:
+        db.disconnect()
 
 
 if __name__ == "__main__":
