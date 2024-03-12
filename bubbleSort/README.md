bea828c6fcd65715442d2407673b0ab9ce1e1a1e
bonoman@volny.cz <bonoman@volny.cz>
2024-03-12 19:27:29 +0100

    Init commit 12.3.2024

---
+++ bubbleSort/README.md
@@ -0,0 +1,8 @@
+Bubble sort algorithm:
+______________________________________________________________________
+
+Demonstration of a bubble sorting algorythm.
+It is based on comparing values of a numbers in a pair.
+In one cycle it compares two values of every pair in a list and places 
+bigger number to end of the list.
+Similar like big bubbles raising in lemonade to it's level.
---
+++ bubbleSort/bubbleSort.py
@@ -0,0 +1,43 @@
+import random
+import time
+
+def genRandInt(interval) -> int:
+    return random.randint(0, interval)
+
+
+def getRandList(interval) -> list:
+    return [genRandInt(interval) for _ in range(0, interval)]
+
+
+def getMax(first, second):
+    retVal = second
+    if first > second:
+        retVal = first
+    return retVal
+
+
+def bubbleSort(sortList) -> list:
+    '''returns sorted list according bubble sort algorythm'''
+    lstLen = len(sortList)
+    haveToSort = True
+    while haveToSort == True:
+        nextIndex = 1
+        haveToSort = False
+        for currentIndex in range(0, lstLen - 1):
+            if nextIndex <= (lstLen - 2):
+                nextIndex = currentIndex + 1
+                if(sortList[currentIndex] > sortList[nextIndex]): # sorting
+                    haveToSort = True
+                    sortList.insert(nextIndex, sortList.pop(currentIndex))
+    return sortList
+
+
+length = 100
+unorderedList = getRandList(length)
+
+#print(f"\n### Unordered list:\n\n{unorderedList}") 
+
+startTime = time.time()
+print(f"\n### Sorted list:\n{bubbleSort(unorderedList)}")
+endTime = time.time()
+print(f"\nSorting duration: {round(endTime - startTime, 3)} [seconds]")
